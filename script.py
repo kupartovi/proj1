d2fa1183767652a9543e298bc9ae9c4467add109
Kurosh Partovi <kurosh@Kuroshs-MacBook-Pro.local>
2020-04-12 13:56:22 -0400

    comment add

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFilter
+#
 img = Image.open('./source.jpg')
 img.thumbnail((400,200)) # this function keeps the aspect ratio even if you give it a value without aspect ratio
 img.save('dest.jpg')
